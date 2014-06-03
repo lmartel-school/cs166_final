662a207b29bccef2d7607f25b768c02f01239b7d
Leo Martel <lmartel@github>
2014-06-03 04:49:27 -0700

    it works!!(?)

--- rope.cpp
+++ rope.cpp
@@ -2,6 +2,7 @@
 #include "splay_tree.h"
 #include <assert.h>
 #include <stack>
+#include <iostream>
 
 typedef SplayTree::TreeNode TreeNode;
 using namespace std;
@@ -19,26 +20,31 @@ Rope::~Rope(){
 }
 
 int Rope::length(){
+	// cout << tree->toString() << endl;
 	return sumWeightsDownRightSpine(tree->root);
 }
 
 char Rope::at(int index){
-	int *localIndex = NULL;
+	int localIndex;
 	TreeNode *node = this->findNodeWithCharAtIndex(this->tree->root, index, localIndex);
-	return node->value->at(*localIndex);
+	return node->value->at(localIndex);
 }
 
 string Rope::report(int start, int end){
-	int *localStart = NULL;
-	int *localEnd = NULL;
+	int localStart;
+	int localEnd;
 	TreeNode *startNode = this->findNodeWithCharAtIndex(this->tree->root, start, localStart);
 	TreeNode *endNode = this->findNodeWithCharAtIndex(this->tree->root, end, localEnd);
+	if (startNode == endNode){
+		return startNode->value->substr(start, end - start);
+	}
 
-	TreeNode *lca = this->findNodeToReport(this->tree->root, start, end);
-	
-	string res = startNode->value->substr(*localStart);
+	TreeNode *lca = this->findNodeToReport(tree->root, start, end);
+	// cout << "lca:" << lca->toString() << endl;
+
+	string res = startNode->value->substr(localStart);
 	res += inOrder(lca, startNode, endNode);
-	res += endNode->value->substr(0, 1 + *localEnd);
+	res += endNode->value->substr(0, 1 + localEnd);
 
 	lca->splay(tree);
 
@@ -48,19 +54,24 @@ string Rope::report(int start, int end){
 Rope *Rope::concat(Rope *right){
 	Rope *left = this;
 	TreeNode *newRoot = new TreeNode(NULL, left->length(), new string(""));
-	newRoot->left = left->tree->root;
-	newRoot->right = right->tree->root;
+	newRoot->setLeft(left->tree->root);
+	newRoot->setRight(right->tree->root);
 
 	return new Rope(new SplayTree(newRoot));
 }
 
 pair<Rope *, Rope *> Rope::split(int index){
-	int *localIndex = NULL;
+	if (tree->root == NULL) return make_pair(new Rope(new SplayTree(NULL)), new Rope(new SplayTree(NULL)));
+	if (index < 0) return make_pair(new Rope(new SplayTree(NULL)), this);
+	int localIndex;
 	TreeNode *node = this->findNodeWithCharAtIndex(this->tree->root, index, localIndex);
+	// cout << node->toString();
+	
 	string *fullString = node->value;
 	int fullLength = fullString->length();
-	int leftLength = *localIndex + 1;
+	int leftLength = localIndex + 1;
 	int rightLength = fullLength - leftLength;
+	// cout << leftLength << " vs " << fullLength << endl;
 	assert(leftLength <= fullLength);
 
 	// If char before split is not the last char in a node, we must split that node
@@ -69,10 +80,13 @@ pair<Rope *, Rope *> Rope::split(int index){
 		newNode->left = new TreeNode(newNode, leftLength, new string(fullString->substr(0, leftLength)));
 		newNode->right = new TreeNode(newNode, rightLength, new string(fullString->substr(leftLength)));
 
-		newNode->parent->replaceChild(node, newNode);
-		delete node;
+		if (newNode->parent != NULL) newNode->parent->replaceChild(node, newNode);
+		// delete node;
 		node = newNode->left;
+		// cout << newNode->toString() << endl;
 	}
+
+	// cout << node->toString() << endl;
 
 	int weightSliced = 0;
 	stack<Rope *> slicedRoots;
@@ -80,20 +94,27 @@ pair<Rope *, Rope *> Rope::split(int index){
 	// Do the split (see https://en.wikipedia.org/wiki/Rope_(computer_science)#Split)
 	TreeNode *cur = node;
 	while (cur->parent != NULL){
-		TreeNode *parent = cur->parent;
-		if (cur == parent->right) break;
+		TreeNode *up = cur->parent;
+		if (cur == up->right) break;
 
-		parent->key -= weightSliced;
-		TreeNode *toSlice = parent->right;
+		up->key -= weightSliced;
+		TreeNode *toSlice = up->right;
 		if (toSlice != NULL){
-			parent->right = NULL;
+			up->right = NULL;
 			toSlice->parent = NULL;
 			slicedRoots.push(new Rope(new SplayTree(toSlice)));
 			weightSliced += sumWeightsDownRightSpine(toSlice);
 		}
 
-		cur = parent;
+		cur = up;
 	}
+
+	// Find the new root of the old tree
+	while (cur->parent != NULL) cur = cur->parent;
+	this->tree->root = cur;
+
+	// If we're splitting at the end of the string, append an empty rope
+	if (slicedRoots.empty()) slicedRoots.push(new Rope(new SplayTree(NULL)));
 
 	// Merge sliced pieces into single rope
 	while (slicedRoots.size() > 1){
@@ -108,35 +129,39 @@ pair<Rope *, Rope *> Rope::split(int index){
 }
 
 Rope *Rope::insert(int index, string *str){
-	pair<Rope *, Rope *> pieces = this->split(index);
 	Rope *newPiece = new Rope(new SplayTree(new TreeNode(NULL, str->length(), str)));
+	if (tree->root == NULL) return newPiece;
+	pair<Rope *, Rope *> pieces = this->split(index - 1);
 	return pieces.first->concat(newPiece)->concat(pieces.second);
 }
 
 Rope *Rope::remove(int start, int end){
 	pair<Rope *, Rope *> pieces = this->split(end);
 	Rope *after = pieces.second;
-	pieces = pieces.first->split(start);
-	delete pieces.second;
+	pieces = pieces.first->split(start - 1);
+	// cout << pieces.first->toString() << pieces.second->toString() << endl;
+	// delete pieces.second;
 	return pieces.first->concat(after);
+}
+
+string Rope::toString(){
+	return tree->toString();
 }
 
 // Begin private helpers
 
-TreeNode *Rope::findNodeWithCharAtIndex(TreeNode *node, int index, int *reportLocalIndex){
+TreeNode *Rope::findNodeWithCharAtIndex(TreeNode *node, int index, int & reportLocalIndex){
 	assert(node != NULL);
-	if (node->key < index){
+	if (node->key <= index && node->right != NULL){
 		return this->findNodeWithCharAtIndex(node->right, index - node->key, reportLocalIndex);
 	} else if (node->left != NULL){
 		return this->findNodeWithCharAtIndex(node->left, index, reportLocalIndex);
 	} else {
 		if (node->parent != NULL){
-
 			// Rebalance tree as much as possible without ripping leaves
 			node->parent->splay(this->tree);
 		}
-
-		*reportLocalIndex = index;
+		reportLocalIndex = index;
 		return node;
 	}
 }
@@ -157,6 +182,7 @@ string Rope::inOrder(TreeNode *node, TreeNode *start, TreeNode *end){
 			if (node == end) return result;
 			if (started && node->value != NULL) result += *(node->value);
 			if (node == start) started = true;
+			node = node->right;
 		}
 		else {
 			toVisit.push(node);
@@ -173,7 +199,11 @@ TreeNode *Rope::findNodeToReport(TreeNode *node, int start, int end){
 		return findNodeToReport(node->right, start - node->key, end - node->key);
 	}
 	else if (start < node->key && end < node->key){
-		return findNodeToReport(node->left, start, end);
+		if (node->left != NULL){
+			return findNodeToReport(node->left, start, end);
+		} else {
+			return node;
+		}
 	}
 	else {
 		return node;
